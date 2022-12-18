2136b104547e4f71c59ba43dac3c478b65167561
kats <lee.kats@gmail.com>
2022-12-18 16:03:15 +0800

    Rebase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import gen_bulletin
 
 
-watch = "/Users/Kats/Documents/TickerManagementSystem/Python/"
+'watch = "/Users/Kats/Documents/TickerManagementSystem/Python/"
+watch = "/home/kats/TMS/
 gen_bulletin.read_excel(watch + "TMS_real case_20221207_v2.xlsx")
